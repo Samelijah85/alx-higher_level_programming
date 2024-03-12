19f3f4dd6ef72cb05eb25b019b224f90392daa74
EvanieWares <evaniewares@gmail.com>
2024-03-12 03:22:54 +0000

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (argv[3]) {
+  console.log('Arguments found');
+} else if (argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
